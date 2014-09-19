aa53cbeb733114489860bd3b55c9a61242fe48b4
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-09-19 18:21:15 +0800

    home

--- json2xls.py
+++ json2xls.py
@@ -3,15 +3,38 @@
 import json
 from xlwt import Workbook, XFStyle, Style, Font, Pattern
 
+XLS_COLORS = [
+    'gray_ega', 'dark_green', 'indigo',
+    'gold', 'blue_grey', 'dark_green_ega',
+    'lavender', 'yellow', 'purple_ega',
+    'olive_ega', 'olive_green', 'light_yellow',
+    'pale_blue', 'violet', 'dark_red_ega',
+    'cyan_ega', 'dark_blue', 'blue_gray',
+    'magenta_ega', 'lime', 'blue', 'grey40',
+    'pink', 'grey25', 'rose', 'white', 'black',
+    'silver_ega', 'gray50', 'periwinkle',
+    'sea_green', 'orange', 'red', 'grey80',
+    'dark_teal', 'brown', 'ivory', 'bright_green',
+    'ocean_blue', 'dark_blue_ega', 'dark_yellow',
+    'light_turquoise', 'light_blue', 'dark_purple',
+    'ice_blue', 'light_orange', 'grey50', 'grey_ega',
+    'tan', 'sky_blue', 'gray25', 'gray40', 'coral',
+    'light_green', 'aqua', 'dark_red', 'gray80',
+    'green', 'teal_ega', 'teal', 'plum', 'turquoise'
+]
+
 
 class Json2Xls(object):
 
-    def __init__(self, json_data, sheet_name='sheet0', title_color='periwinkle', font_name='Verdana'):
+    def __init__(self, json_data, sheet_name='sheet0', title_color='lime', font_name='Arial'):
         self.sheet_name = sheet_name
         self.json_data = json_data
         self.book = Workbook(encoding='utf-8')
         self.sheet = self.book.add_sheet(self.sheet_name)
         self.title_color = title_color
+        self.title_start_col = 0
+        self.title_start_row = 0
+        self.title_merge_info = {}
 
         self.font = Font()
         self.font.name = font_name
@@ -23,16 +46,29 @@ class Json2Xls(object):
         self.title_style.font = self.font
         self.title_style.pattern = self.pattern
 
+
+
+    def __parse_merge_info(self, data, parent=None):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                value_len = len(value)
+                if parent and value_len > self.title_merge_info[parent]['len']:
+                    self.title_merge_info[parent]['len'] = value_len
+                else:
+                    self.title_merge_info.update({key: {'len': len(value), 'parent':parent}})
+                    self.__parse_merge_info(value, parent=key)
+
     def __genarate_title(self, data):
-        start_col = 0
-        row = 0
-        for key_name in data.keys():
-            end_col = len(data[key_name])
-            self.sheet.write_merge(row, row, start_col, start_col + end_col - 1, key_name, self.title_style)
-            start_col += end_col
-            if isinstance(data[key_name], dict):
-                row += 1
-                self.__genarate_title(data[key_name])
+        for key_col, key_name in enumerate(data.keys()):
+            self.sheet.write_merge(
+                self.title_start_row,
+                self.title_start_row,
+                self.title_start_col,
+                self.title_start_col + self.title_merge_len - 1,
+                key_name,
+                self.title_style
+            )
+            self.title_start_col += self.title_merge_len
 
     def json2xls(self):
         data = json.loads(self.json_data)
@@ -41,11 +77,10 @@ class Json2Xls(object):
         if isinstance(data, dict):
             data = [data]
 
-
-
         self.__genarate_title(data[0])
         self.book.save("test.xls")
 
-json_data = '{"title": {"tag": "im title tag", "ner": "im title ner"}, "body":{"tag": "im body tag", "ner": "im body ner"}}'
-j = Json2Xls(json_data)
-j.json2xls()
+#json_data = '{"title": {"tag": "im title tag", "ner": "im title ner"}, "body":{"tag": "im body tag", "ner": "im body ner"}}'
+#j = Json2Xls(json_data)
+#j.json2xls()
+
--- tests.py
+++ tests.py
@@ -1,161 +1,26 @@
-from xlwt import *
-
-fnt = Font()
-fnt.name = 'Arial'
-fnt.colour_index = 4
-fnt.bold = True
-
-borders = Borders()
-borders.left = 6
-borders.right = 6
-borders.top = 6
-borders.bottom = 6
-
-al = Alignment()
-al.horz = Alignment.HORZ_CENTER
-al.vert = Alignment.VERT_CENTER
-
-style = XFStyle()
-style.font = fnt
-style.borders = borders
-style.alignment = al
+from treelib import Tree, Node
 
 
-wb = Workbook()
-ws0 = wb.add_sheet('sheet0')
-ws1 = wb.add_sheet('sheet1')
-ws2 = wb.add_sheet('sheet2')
+data = {
+    "a": {"a1": "va1"},
+    "b": {"b1": "vb1", "b2": "vb2"},
+    "c": {"c1": "vc1", "c2": "vc2", 'c3': 'vc3'},
+    "d": {"d1": {'dd1': 'vdd1', 'dd2': 'vdd2', 'dd3': 'vdd3'}, 'd2': 'vd2'},
+}
 
-for i in range(0, 0x200, 2):
-    ws0.write_merge(i, i+1, 1, 5, 'test %d' % i, style)
-    ws1.write_merge(i, i, 1, 7, 'test %d' % i, style)
-    ws2.write_merge(i, i+1, 1, 7 + (i%10), 'test %d' % i, style)
+d = {}
 
+def parse_dict(data, parent=None):
+    for key, value in data.items():
+        if isinstance(value, dict):
+            # {'a': {'parent': None, 'len': 1}, 'c': {'parent': None, 'len': 3}, 'b': {'parent': None, 'len': 2}, 'd': {'parent': None, 'len': 2}, 'd1': {'parent': 'd', 'len': 3}}
+            value_len = len(value)
+            print value_len
+            if parent and value_len > d[parent]['len']:
+                d[parent]['len'] = value_len
+            else:
+                d.update({key: {'len': len(value), 'parent':parent}})
+                parse_dict(value, parent=key)
 
-wb.save('merged.xls')
-
-from xlwt import *
-
-wb = Workbook()
-ws0 = wb.add_sheet('sheet0')
-
-
-fnt = Font()
-fnt.name = 'Arial'
-fnt.colour_index = 4
-fnt.bold = True
-
-borders = Borders()
-borders.left = 6
-borders.right = 6
-borders.top = 6
-borders.bottom = 6
-
-style = XFStyle()
-style.font = fnt
-style.borders = borders
-
-ws0.write_merge(3, 3, 1, 5, 'test1', style)
-ws0.write_merge(4, 10, 1, 5, 'test2', style)
-ws0.col(1).width = 0x0d00
-
-wb.save('merged0.xls')
-
-from xlwt import *
-
-wb = Workbook()
-ws0 = wb.add_sheet('sheet0')
-
-fnt1 = Font()
-fnt1.name = 'Verdana'
-fnt1.bold = True
-fnt1.height = 18*0x14
-
-pat1 = Pattern()
-pat1.pattern = Pattern.SOLID_PATTERN
-pat1.pattern_fore_colour = 0x16
-
-brd1 = Borders()
-brd1.left = 0x06
-brd1.right = 0x06
-brd1.top = 0x06
-brd1.bottom = 0x06
-
-fnt2 = Font()
-fnt2.name = 'Verdana'
-fnt2.bold = True
-fnt2.height = 14*0x14
-
-brd2 = Borders()
-brd2.left = 0x01
-brd2.right = 0x01
-brd2.top = 0x01
-brd2.bottom = 0x01
-
-pat2 = Pattern()
-pat2.pattern = Pattern.SOLID_PATTERN
-pat2.pattern_fore_colour = 0x01F
-
-fnt3 = Font()
-fnt3.name = 'Verdana'
-fnt3.bold = True
-fnt3.italic = True
-fnt3.height = 12*0x14
-
-brd3 = Borders()
-brd3.left = 0x07
-brd3.right = 0x07
-brd3.top = 0x07
-brd3.bottom = 0x07
-
-fnt4 = Font()
-
-al1 = Alignment()
-al1.horz = Alignment.HORZ_CENTER
-al1.vert = Alignment.VERT_CENTER
-
-al2 = Alignment()
-al2.horz = Alignment.HORZ_RIGHT
-al2.vert = Alignment.VERT_CENTER
-
-al3 = Alignment()
-al3.horz = Alignment.HORZ_LEFT
-al3.vert = Alignment.VERT_CENTER
-
-style1 = XFStyle()
-style1.font = fnt1
-style1.alignment = al1
-style1.pattern = pat1
-style1.borders = brd1
-
-style2 = XFStyle()
-style2.font = fnt2
-style2.alignment = al1
-style2.pattern = pat2
-style2.borders = brd2
-
-style3 = XFStyle()
-style3.font = fnt3
-style3.alignment = al1
-style3.pattern = pat2
-style3.borders = brd3
-
-price_style = XFStyle()
-price_style.font = fnt4
-price_style.alignment = al2
-price_style.borders = brd3
-price_style.num_format_str = '_(#,##0.00_) "money"'
-
-ware_style = XFStyle()
-ware_style.font = fnt4
-ware_style.alignment = al3
-ware_style.borders = brd3
-
-
-ws0.merge(3, 3, 1, 5, style1)
-ws0.merge(4, 10, 1, 6, style2)
-ws0.merge(14, 16, 1, 7, style3)
-ws0.col(1).width = 0x0d00
-
-
-wb.save('merged1.xls')
+parse_dict(data)
+print d
