3ea6f2deac108ccddd9d240248022aef68543e87
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-09-22 15:42:42 +0800

    just depth 1

--- json2xls.py
+++ json2xls.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import json
+import requests
 from xlwt import Workbook, XFStyle, Style, Font, Pattern, Borders
 
 XLS_COLORS = [
@@ -26,16 +27,20 @@ XLS_COLORS = [
 
 class Json2Xls(object):
 
-    def __init__(self, json_data, sheet_name='sheet0', title_color='lime', font_name='Arial'):
+    def __init__(self, url_or_json, method='get', params=None, data=None, headers=None, sheet_name='sheet0', title_color='lime', font_name='Arial'):
         self.sheet_name = sheet_name
-        self.json_data = json_data
+        self.url_or_json = url_or_json
+        self.method = method
+        self.params = params
+        self.data = data
+        self.headers = headers
+
         self.book = Workbook(encoding='utf-8')
         self.sheet = self.book.add_sheet(self.sheet_name)
         self.title_color = title_color
         self.title_start_col = 0
         self.title_start_row = 0
         self.title_merge_info = {}
-        self.data_start_row = 0
 
         self.font = Font()
         self.font.name = font_name
@@ -56,57 +61,62 @@ class Json2Xls(object):
         self.title_style.borders = self.borders
         self.title_style.pattern = self.pattern
 
+    def __parse_dict_depth(self, d, depth=0):
+        if not isinstance(d, dict) or not d:
+            return depth
+        return max(self.__parse_dict_depth(v, depth+1) for k, v in d.iteritems())
 
+    def __check_dict_deep(self, d):
+        depth = self.__parse_dict_depth(d)
+        if depth > 1:
+            raise Exception("dict is too deep")
 
-    def __parse_merge_info(self, data, parent=None):
-        for key, value in data.items():
-            if isinstance(value, dict):
-                value_len = len(value)
-                if value_len > len(data[key]):
-                    self.title_merge_info[key]['len'] = value_len
+    def __get_json(self):
+        data = None
+        try:
+            data = json.loads(self.url_or_json)
+        except:
+            try:
+                if self.method.lower() == 'get':
+                    resp = requests.get(self.url_or_json, params=self.params, headers=self.headers)
+                    data = resp.json()
                 else:
-                    self.title_merge_info.update({key: len(value)})
-                    self.__parse_merge_info(value, parent=key)
-            else:
-                if not parent:
-                    self.title_merge_info.update({key: 1})
+                    resp = requests.post(self.url_or_json, data=self.data, headers=self.headers)
+                    data = resp.json()
+            except Exception as e:
+                print e
+        return data
 
-    def __genarate_title(self, data):
-        self.__parse_merge_info(data)
-        for key_col, key_name in enumerate(data.keys()):
-            self.sheet.write_merge(
-                self.title_start_row,
-                self.title_start_row,
-                self.title_start_col,
-                self.title_start_col + self.title_merge_info[key_name] - 1,
-                key_name,
-                self.title_style
-            )
-            self.title_start_col += self.title_merge_info[key_name]
+    def __fill_title(self, data):
+        self.__check_dict_deep(data)
+        for index, key in enumerate(data.keys()):
+            self.sheet.row(self.title_start_row).write(index, key, self.title_style)
+        self.title_start_row += 1
 
     def __fill_data(self, data):
-        for index, key_name in enumerate(self.title_merge_info.keys()):
-            if isinstance(data[key_name], dict):
-                for index1, value in enumerate(data[key_name].values()):
-                    self.sheet.row(self.data_start_row).write(index1 + index, value)
-            else:
-                self.sheet.row(self.data_start_row).write(index, data[key_name])
-        self.data_start_row += 1
+        self.__check_dict_deep(data)
+        for index, value in enumerate(data.values()):
+            self.sheet.row(self.title_start_row).write(index, value)
+
+        self.title_start_row += 1
 
     def make(self):
-        data = json.loads(self.json_data)
+        data = self.__get_json()
         if not isinstance(data, (dict, list)):
             raise Exception('bad json format')
         if isinstance(data, dict):
             data = [data]
 
-        self.__genarate_title(data[0])
-        self.data_start_row = len(self.title_merge_info) - 1
+        self.__fill_title(data[0])
         for d in data:
             self.__fill_data(d)
         self.book.save("test.xls")
 
-json_data = '[{"title": {"tag": "im xtitle tag", "ner": "im xtitle ner"}, "body":{"tag": "im xbody tag"}}, {"title": {"tag": "im title tag", "ner": "im title ner"}, "body":{"tag": "im body tag"}}]'
-j = Json2Xls(json_data)
+url_or_json = '''[{"name": "John", "age": 30,
+  "sex": "male"},
+ {"name": "Alice", "age": 18,
+  "sex": "female"}
+]'''
+j = Json2Xls("http://142.4.209.29:9080/ask/%E6%88%91%E6%83%B3%E8%87%AA%E6%9D%80")
 j.make()
 
