384635cacc8b0716f2104ce0a2d7dc48bbd6795f
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-09-24 18:43:08 +0800

    update title style

--- json2xls/json2xls.py
+++ json2xls/json2xls.py
@@ -4,37 +4,15 @@ import json
 import requests
 import os
 import click
-from xlwt import Workbook, XFStyle, Style, Font, Pattern, Borders
-
-XLS_COLORS = [
-    'gray_ega', 'dark_green', 'indigo',
-    'gold', 'blue_grey', 'dark_green_ega',
-    'lavender', 'yellow', 'purple_ega',
-    'olive_ega', 'olive_green', 'light_yellow',
-    'pale_blue', 'violet', 'dark_red_ega',
-    'cyan_ega', 'dark_blue', 'blue_gray',
-    'magenta_ega', 'lime', 'blue', 'grey40',
-    'pink', 'grey25', 'rose', 'white', 'black',
-    'silver_ega', 'gray50', 'periwinkle',
-    'magenta_ega', 'lime', 'blue', 'grey40',
-    'pink', 'grey25', 'rose', 'white', 'black',
-    'silver_ega', 'gray50', 'periwinkle',
-    'sea_green', 'orange', 'red', 'grey80',
-    'dark_teal', 'brown', 'ivory', 'bright_green',
-    'ocean_blue', 'dark_blue_ega', 'dark_yellow',
-    'light_turquoise', 'light_blue', 'dark_purple',
-    'ice_blue', 'light_orange', 'grey50', 'grey_ega',
-    'tan', 'sky_blue', 'gray25', 'gray40', 'coral',
-    'light_green', 'aqua', 'dark_red', 'gray80',
-    'green', 'teal_ega', 'teal', 'plum', 'turquoise'
-]
+import xlwt
+from xlwt import Workbook
 
 
 class Json2Xls(object):
 
     def __init__(self, filename, url_or_json, method='get',
                  params=None, data=None, headers=None,
-                 sheet_name='sheet0', title_color='lime', font_name='Arial'):
+                 sheet_name='sheet0', title_style=None):
         self.sheet_name = sheet_name
         self.filename = filename
         self.url_or_json = url_or_json
@@ -42,39 +20,20 @@ class Json2Xls(object):
         self.params = params
         self.data = data
         self.headers = headers
-        self.title_color = title_color
 
-        self.__check_color()
         self.__check_file_suffix()
 
         self.book = Workbook(encoding='utf-8')
         self.sheet = self.book.add_sheet(self.sheet_name)
 
-        self.title_start_col = 0
         self.title_start_row = 0
 
-        self.font = Font()
-        self.font.name = font_name
-        self.font.bold = True
-
-        self.borders = Borders()
-        self.borders.left = 1
-        self.borders.right = 1
-        self.borders.top = 1
-        self.borders.bottom = 1
-
-        self.pattern = Pattern()
-        self.pattern.pattern = Pattern.SOLID_PATTERN
-        self.pattern.pattern_fore_colour = Style.colour_map[self.title_color]
-
-        self.title_style = XFStyle()
-        self.title_style.font = self.font
-        self.title_style.borders = self.borders
-        self.title_style.pattern = self.pattern
-
-    def __check_color(self):
-        if self.title_color not in XLS_COLORS:
-            raise Exception('your color is not supported')
+        self.title_style = xlwt.easyxf(title_style or
+                                       'font: name Arial, bold on;'
+                                       'align: vert centre, horiz center;'
+                                       'borders: top 1, bottom 1, left 1, right 1;'
+                                       'pattern: pattern solid, fore_colour lime;'
+                                       )
 
     def __check_file_suffix(self):
         suffix = self.filename.split('.')[-1]
@@ -154,14 +113,13 @@ class Json2Xls(object):
 @click.option('--data', '-d', default=None)
 @click.option('--headers', '-h', default=None)
 @click.option('--sheet', '-s', default='sheet0')
-@click.option('--color', '-c', default='lime')
-@click.option('--font', '-f', default='Arial')
-def make(filename, source, method, params, data, headers, sheet, color, font):
+@click.option('--style', '-t', default=None)
+def make(filename, source, method, params, data, headers, sheet, style):
     if isinstance(headers, basestring):
         headers = eval(headers)
     Json2Xls(filename, source, method=method, params=params,
              data=data, headers=headers, sheet_name=sheet,
-             title_color=color, font_name=font).make()
+             title_style=style).make()
 
 if __name__ == '__main__':
     make()
